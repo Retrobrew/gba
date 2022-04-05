52e166fb1131a34537b430c80ebf6b8fc60bd3f1
LEPROHON Cédric <noreply@retrobrew.fr>
2022-04-05 10:32:14 +0200

    fix: make_examples

--- make_example.sh
+++ make_example.sh
@@ -4,8 +4,7 @@ if [ "$1" = "" ]; then
   echo "Usage: $0 [example to build]"
   exit 1
 fi
-
 cargo build --example $1 --release || exit 1
-arm-none-eabi-objcopy -O binary target/thumbv4-none-agb/release/examples/$1 target/$1.gba || exit 1
+arm-none-eabi-objcopy -O binary target/thumbv4t-none-eabi/release/examples/$1 target/$1.gba || exit 1
 gbafix target/$1.gba || exit 1
 echo "ROM built successfully!"
